06822cdb9fb28ee84f56bd1bc71276d6a97fd869
Paroubek Petr <Petr.Paroubek@cdl.cz>
2018-08-07 08:09:16 +0200

    joins

--- operationTask.js
+++ operationTask.js
@@ -17,6 +17,26 @@ class DatabaseTasks{
           console.log("Connected!");
         });
       }
+      selectData(){
+        var DatCon = this.DatabeseConn();
+        DatCon.connect(function(err) {
+          if (err) throw err;
+          DatCon.query("SELECT * FROM table_users", function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+          });
+        });
+      }
+      joinData(){
+        var DatCon = this.DatabeseConn();
+        DatCon.connect(function(err) {
+          if (err) throw err;
+          DatCon.query("SELECT table_users.username, rented_car.car_name FROM table_users LEFT JOIN rented_car ON table_users.id = rented_car.rented_by", function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+          });
+        });
+      }
 }
 DataOper = new DatabaseTasks();
-DataOper.TryConn();
+DataOper.joinData();
